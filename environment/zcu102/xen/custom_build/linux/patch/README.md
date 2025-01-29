329fc3444b811d34b2273408f5d4adb0ea8d0cdb
Daniele Ottaviano <daniele.ottaviano@tum.de>
2025-01-29 11:10:47 +0100

    Created Xen Directory and output for the zcu102
    Signed-off-by: Daniele Ottaviano <daniele.ottaviano@tum.de>

---
+++ environment/zcu102/xen/custom_build/linux/patch/README.md
@@ -0,0 +1,2 @@
+
+- preempt-rt: The patch enable the fully preemption mode in the kernel (https://wiki.linuxfoundation.org/realtime/start).
---
+++ environment/zcu102/xen/output/boot/sources/boot_tftp.cmd
@@ -0,0 +1,18 @@
+tftpb 0xE00000 xen-Image
+tftpb 0x2600000 xen-rootfs.cpio.gz
+tftpb 0x6600000 xen
+tftpb 0x6800000 xen.dtb
+fdt addr 0x6800000
+fdt resize 1024
+fdt set /chosen \#address-cells <0x2>
+fdt set /chosen \#size-cells <0x2>
+fdt set /chosen xen,xen-bootargs "console=dtuart dtuart=serial0 dom0_mem=1500M dom0_max_vcpus=1 bootscrub=0 vwfi=native sched=null   "
+fdt mknod /chosen dom0
+fdt set /chosen/dom0 compatible  "xen,linux-zimage" "xen,multiboot-module" "multiboot,module"
+fdt set /chosen/dom0 reg <0x0 0xE00000 0x0 0x1725200 >
+fdt set /chosen xen,dom0-bootargs "console=hvc0 earlycon=xen earlyprintk=xen clk_ignore_unused root=/dev/ram0"
+fdt mknod /chosen dom0-ramdisk
+fdt set /chosen/dom0-ramdisk compatible  "xen,linux-initrd" "xen,multiboot-module" "multiboot,module"
+fdt set /chosen/dom0-ramdisk reg <0x0 0x2600000 0x0 0x3EC3749 >
+setenv fdt_high 0xffffffffffffffff
+booti 0x6600000 - 0x6800000
